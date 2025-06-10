04ee96959a7590ea53077e4abce4acc152fc3c3a
JavierCaceres5 <00068223@uca.edu.sv>
2025-06-09 22:58:08 -0600

    Logica de login

--- Frontend/app/src/main/java/com/poyecto/ReUbica/MainActivity.kt
+++ Frontend/app/src/main/java/com/poyecto/ReUbica/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.proyecto.ReUbica
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 import com.poyecto.ReUbica.ui.navigations.MainNavigation
 import com.proyecto.ReUbica.ui.theme.ReUbicaTheme
 
@@ -19,4 +22,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
