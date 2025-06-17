6d911e1a0317d36bddb2a96016f9a29ee4f0ee28
Marceloereyes <00148023@uca.edu.sv>
2025-06-16 20:11:16 -0600

    Version Final

--- ProyectoPDM/app/src/main/java/com/poyecto/ReUbica/product/ProductResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.proyecto.ReUbica.product
-
-sealed interface ProductResponse {
-    data class Success(val data: List<Product>? = null) : ProductResponse
-    data class Error(val message: String?) : ProductResponse
-}
